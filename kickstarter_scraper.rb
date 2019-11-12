2e5dee89343a574bf63c2bdb86037081fdded606
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-12 00:42:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,11 @@
 # require libraries/modules here
 
-def create_project_hash
+
+
   html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+def create_project_hash
+  
 
 end
